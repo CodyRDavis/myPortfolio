530e415621d56b09d0d1596f969a59fdc3fb3698
Cody Davis <codydavis@codys-imac.lan>
2018-11-19 14:29:53 -0600

    Bringing portfolio links online

--- javascript/nav.js
+++ javascript/nav.js
@@ -15,3 +15,45 @@ document.getElementById('buttonMenu').addEventListener('click', function(event){
     }
 
 });
+
+//SOCIAL NAVBUTTONS
+document.getElementById('button-twitter').addEventListener('click', function(event){
+    window.open("https://twitter.com/WizardSchnoz");
+});
+
+document.getElementById('button-github').addEventListener('click', function(event){
+    window.open("https://github.com/CodyRDavis");
+});
+
+document.getElementById('button-linkedIn').addEventListener('click', function(event){
+    window.open("https://www.linkedin.com/in/cody-r-davis/");
+});
+
+//PORTFOLIO LINKS
+document.getElementById('button-dicebag').addEventListener('click', function(event){
+    window.open("https://codyrdavis.github.io/DiceBag/");
+});
+document.getElementById('button-essentals').addEventListener('click', function(event){
+    window.open("https://jjmonte20.github.io/project1Team2/");
+});
+document.getElementById('button-giphy').addEventListener('click', function(event){
+    window.open("https://codyrdavis.github.io/AJAXexample/");
+});
+document.getElementById('button-getout').addEventListener('click', function(event){
+    window.open("https://codyrdavis.github.io/timerGame/");
+});
+document.getElementById('button-rpg').addEventListener('click', function(event){
+    window.open("https://codyrdavis.github.io/jQueryGame/");
+});
+document.getElementById('button-hangman').addEventListener('click', function(event){
+    window.open("https://codyrdavis.github.io/guessLetter/");
+});
+document.getElementById('button-bootstrap').addEventListener('click', function(event){
+    window.open("https://codyrdavis.github.io/Bootstrap-Portfolio/");
+});
+
+/*  BLANK BUTTON LISTENER
+document.getElementById('button-').addEventListener('click', function(event){
+    window.open("");
+}); 
+*/
